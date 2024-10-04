b578f6569024ad2011c46230749787d878847796
Tim Pambor <t.pambor@uniandes.edu.co>
2024-10-05 00:43:41 +0200

    Add comments to terraform configuration

--- terraform/circleci.tf
+++ terraform/circleci.tf
@@ -1,3 +1,5 @@
+# Grants the CircleCI service account the ability to act as the service account of this microservice.
+# This is required for CircleCI to be able to deploy the service to Cloud Run.
 resource "google_project_iam_member" "circleci_cloud_run_service_account" {
   project = local.project_id
   role    = "roles/iam.serviceAccountUser"
--- terraform/secret.tf
+++ terraform/secret.tf
@@ -1,9 +1,12 @@
+# Enables the Secret Manager API for the project.
 resource "google_project_service" "secretmanager" {
   service = "secretmanager.googleapis.com"
 
+  # Prevents the API from being disabled when the resource is destroyed.
   disable_on_destroy = false
 }
 
+# Creates a Secret Manager secret to store the JWT private key.
 resource "google_secret_manager_secret" "jwt_private_key" {
   secret_id = "jwt-private-key"
   replication {
@@ -13,12 +16,14 @@ resource "google_secret_manager_secret" "jwt_private_key" {
   depends_on = [ google_project_service.secretmanager ]
 }
 
+# Creates a version of the secret containing the JWT private key data.
 resource "google_secret_manager_secret_version" "jwt_private_key" {
   secret = google_secret_manager_secret.jwt_private_key.id
 
   secret_data = local.jwt_private_key
 }
 
+# Grants the service account (this microservice) read access to the JWT private key secret.
 resource "google_secret_manager_secret_iam_member" "read_jwt_private_key" {
   secret_id = google_secret_manager_secret.jwt_private_key.id
   role      = "roles/secretmanager.secretAccessor"
