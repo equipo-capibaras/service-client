dd709702048bbbf84f30c4c23a8035ff6762306f
Tim Pambor <t.pambor@uniandes.edu.co>
2024-10-08 17:35:57 +0200

    Deduplicate code by converting to parametrized test method

--- tests/repositories/firestore/test_employee.py
+++ tests/repositories/firestore/test_employee.py
@@ -1,13 +1,14 @@
 import os
 from dataclasses import asdict
 from typing import cast
-from unittest import TestCase, skipUnless
+from unittest import skipUnless
 
 import requests
 from faker import Faker
 from google.cloud.firestore import Client as FirestoreClient  # type: ignore[import-untyped]
 from google.cloud.firestore_v1 import DocumentReference
 from passlib.hash import pbkdf2_sha256
+from unittest_parametrize import ParametrizedTestCase, parametrize
 
 from models import Client, Employee, Plan, Role
 from repositories.firestore import FirestoreEmployeeRepository
@@ -16,7 +17,7 @@ FIRESTORE_DATABASE = '(default)'
 
 
 @skipUnless('FIRESTORE_EMULATOR_HOST' in os.environ, 'Firestore emulator not available')
-class TestEmployee(TestCase):
+class TestEmployee(ParametrizedTestCase):
     def setUp(self) -> None:
         self.faker = Faker()
 
@@ -29,7 +30,17 @@ class TestEmployee(TestCase):
         self.repo = FirestoreEmployeeRepository(FIRESTORE_DATABASE)
         self.client = FirestoreClient(database=FIRESTORE_DATABASE)
 
-    def test_find_by_email(self) -> None:
+        self.emails = [self.faker.unique.email() for _ in range(4)]
+
+    @parametrize(
+        ('email_idx_map', 'find_idx', 'expected'),
+        [
+            ({0: 0, 1: 1, 2: 2}, 0, 0),  # Employee found
+            ({0: 0, 1: 1, 2: 2}, 3, None),  # Employee not found
+            ({0: 0, 1: 0, 2: 2}, 0, None),  # Multiple employees found
+        ],
+    )
+    def test_find_by_email(self, email_idx_map: dict[int, int], find_idx: int, expected: int | None) -> None:
         client = Client(
             id=cast(str, self.faker.uuid4()),
             name=self.faker.company(),
@@ -41,74 +52,17 @@ class TestEmployee(TestCase):
         del client_dict['id']
         self.client.collection('clients').document(client.id).set(client_dict)
 
-        employee = Employee(
-            id=cast(str, self.faker.uuid4()),
-            client_id=client.id,
-            name=self.faker.name(),
-            email=self.faker.unique.email(),
-            password=pbkdf2_sha256.hash(self.faker.password()),
-            role=self.faker.random_element(list(Role)),
-        )
-        employee_dict = asdict(employee)
-        del employee_dict['id']
-        del employee_dict['client_id']
-        self.client.collection('clients').document(client.id).collection('employees').document(employee.id).set(employee_dict)
-
-        employee_db = self.repo.find_by_email(employee.email)
-        self.assertIsNotNone(employee_db)
-        self.assertEqual(employee_db, employee)
-
-    def test_find_by_email_not_found(self) -> None:
-        client = Client(
-            id=cast(str, self.faker.uuid4()),
-            name=self.faker.company(),
-            plan=self.faker.random_element(list(Plan)),
-            email_incidents=self.faker.unique.email(),
-        )
-
-        client_dict = asdict(client)
-        del client_dict['id']
-        self.client.collection('clients').document(client.id).set(client_dict)
-
-        employee = Employee(
-            id=cast(str, self.faker.uuid4()),
-            client_id=client.id,
-            name=self.faker.name(),
-            email=self.faker.unique.email(),
-            password=pbkdf2_sha256.hash(self.faker.password()),
-            role=self.faker.random_element(list(Role)),
-        )
-        employee_dict = asdict(employee)
-        del employee_dict['id']
-        del employee_dict['client_id']
-        self.client.collection('clients').document(client.id).collection('employees').document(employee.id).set(employee_dict)
-
-        employee_db = self.repo.find_by_email(self.faker.unique.email())
-        self.assertIsNone(employee_db)
-
-    def test_find_by_email_multiple_found(self) -> None:
-        client = Client(
-            id=cast(str, self.faker.uuid4()),
-            name=self.faker.company(),
-            plan=self.faker.random_element(list(Plan)),
-            email_incidents=self.faker.unique.email(),
-        )
-
-        client_dict = asdict(client)
-        del client_dict['id']
-        self.client.collection('clients').document(client.id).set(client_dict)
-
-        email = self.faker.unique.email()
-
-        for _ in range(2):
+        employees: list[Employee] = []
+        for idx, _ in enumerate(range(3)):
             employee = Employee(
                 id=cast(str, self.faker.uuid4()),
                 client_id=client.id,
                 name=self.faker.name(),
-                email=email,
+                email=self.emails[email_idx_map[idx]],
                 password=pbkdf2_sha256.hash(self.faker.password()),
                 role=self.faker.random_element(list(Role)),
             )
+            employees.append(employee)
             employee_dict = asdict(employee)
             del employee_dict['id']
             del employee_dict['client_id']
@@ -116,11 +70,24 @@ class TestEmployee(TestCase):
                 employee_dict
             )
 
-        with self.assertLogs() as cm:
-            employee_db = self.repo.find_by_email(email)
-        self.assertIsNone(employee_db)
-        self.assertEqual(cm.records[0].message, f'Multiple employees found with email {email}')
-        self.assertEqual(cm.records[0].levelname, 'ERROR')
+        duplicate_emails = len(set(email_idx_map.values())) != len(email_idx_map)
+
+        if duplicate_emails:
+            with self.assertLogs() as cm:
+                employee_db = self.repo.find_by_email(self.emails[find_idx])
+        else:
+            with self.assertNoLogs():
+                employee_db = self.repo.find_by_email(self.emails[find_idx])
+
+        if expected is not None:
+            self.assertIsNotNone(employee_db)
+            self.assertEqual(employee_db, employees[expected])
+        else:
+            self.assertIsNone(employee_db)
+
+            if duplicate_emails:
+                self.assertEqual(cm.records[0].message, f'Multiple employees found with email {self.emails[find_idx]}')
+                self.assertEqual(cm.records[0].levelname, 'ERROR')
 
     def test_create(self) -> None:
         client = Client(
