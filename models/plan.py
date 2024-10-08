869613e9d20d91fbd449dd4d94e05a24d0081893
Tim Pambor <t.pambor@uniandes.edu.co>
2024-10-08 16:00:20 +0200

    Fix typo

--- models/plan.py
+++ models/plan.py
@@ -4,4 +4,4 @@ from enum import StrEnum
 class Plan(StrEnum):
     EMPRENDEDOR = 'emprendedor'
     EMPRESARIO = 'empresario'
-    EMPRESARIO_PLUS = 'empresarios_plus'
+    EMPRESARIO_PLUS = 'empresario_plus'
