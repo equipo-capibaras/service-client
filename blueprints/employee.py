911df2e5ec604b9edc9957113b688788a2e9fad6
CamiloRamirezR <c.ramirezr2@uniandes.edu.co>
2024-10-15 18:22:33 -0500

    Se crea el endpoint para registrar empleados

--- blueprints/employee.py
+++ blueprints/employee.py
@@ -1,13 +1,19 @@
+import uuid
 from typing import Any
 
 from dependency_injector.wiring import Provide
-from flask import Blueprint, Response
+from flask import Blueprint, Response, request
 from flask.views import MethodView
+from dataclasses import dataclass, field
+
+from marshmallow import ValidationError
+from passlib.handlers.pbkdf2 import pbkdf2_sha256
 
 from containers import Container
-from models import Employee
-from repositories import EmployeeRepository
-
+from models import Employee, Role
+from repositories import EmployeeRepository, DuplicateEmailError
+import marshmallow.validate
+import marshmallow_dataclass
 from .util import class_route, error_response, json_response, requires_token
 
 blp = Blueprint('Employees', __name__)
@@ -23,15 +29,71 @@ def employee_to_dict(employee: Employee) -> dict[str, Any]:
     }
 
 
+# Employee validation class
+@dataclass
+class RegisterEmployeeBody:
+    name: str = field(metadata={'validate': marshmallow.validate.Length(min=1, max=60)})
+    email: str = field(
+        metadata={'validate': [marshmallow.validate.Email(), marshmallow.validate.Length(min=1, max=60)]})
+    password: str = field(metadata={'validate': marshmallow.validate.Length(min=8)})
+    role: str = field(
+        metadata={'validate': marshmallow.validate.OneOf([Role.ADMIN.value, Role.ANALYST.value, Role.AGENT.value])})
+
+
 @class_route(blp, '/api/v1/employees/me')
 class EmployeeInfo(MethodView):
     init_every_request = False
 
     @requires_token
-    def get(self, token: dict[str, Any], employee_repo: EmployeeRepository = Provide[Container.employee_repo]) -> Response:
+    def get(self, token: dict[str, Any],
+            employee_repo: EmployeeRepository = Provide[Container.employee_repo]) -> Response:
         employee = employee_repo.get(employee_id=token['sub'], client_id=token['cid'])
 
         if employee is None:
             return error_response('Employee not found', 404)
 
         return json_response(employee_to_dict(employee), 200)
+
+
+@class_route(blp, '/api/v1/employees')
+class EmployeeRegister(MethodView):
+    init_every_request = False
+
+    @requires_token
+    def post(self, token: dict[str, Any],
+             employee_repo: EmployeeRepository = Provide[Container.employee_repo]) -> Response:
+
+        # Validate that the user is an admin
+        if token['role'] != Role.ADMIN.value:
+            return error_response('Only administrators can create employees.', 403)
+
+        # Validate request body
+        auth_schema = marshmallow_dataclass.class_schema(RegisterEmployeeBody)()
+        req_json = request.get_json(silent=True)
+
+        if req_json is None:
+            return error_response('Request body must be a JSON object.', 400)
+
+        try:
+            data: RegisterEmployeeBody = auth_schema.load(req_json)
+        except ValidationError as err:
+            return error_response(err.messages, 400)
+
+        # Create employee (without client_id)
+        employee = Employee(
+            id=str(uuid.uuid4()),
+            client_id=None,
+            name=data.name,
+            email=data.email,
+            password=pbkdf2_sha256.hash(data.password),
+            role=Role(data.role),
+        )
+
+        # Save employee
+        try:
+            employee_repo.create(employee)
+        except DuplicateEmailError:
+            return error_response('An employee with the email already exists.', 400)
+
+        # Return success response
+        return json_response(employee_to_dict(employee), 201)
