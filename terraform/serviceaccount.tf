3e76c315759c84499b25a3772eb99c0a41741985
CamiloRamirezR <c.ramirezr2@uniandes.edu.co>
2024-11-10 13:39:51 -0500

    Permisos para ms de factura

--- terraform/serviceaccount.tf
+++ terraform/serviceaccount.tf
@@ -75,3 +75,12 @@ data "google_service_account" "registroapp" {
 
   depends_on = [ google_project_service.iam ]
 }
+
+# Retrieves the service account of the registroapp microservice.
+# This is defined as part of the registroapp microservice
+# This service account is given permissions to access this microservice
+data "google_service_account" "invoice" {
+  account_id   = "invoice"
+
+  depends_on = [ google_project_service.iam ]
+}
