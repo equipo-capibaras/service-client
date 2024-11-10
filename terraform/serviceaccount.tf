5a298b9ef9cce0d218de307269c365ff9d6665b4
Tim Pambor <t.pambor@uniandes.edu.co>
2024-11-10 19:52:00 +0100

    Cleanup

--- terraform/serviceaccount.tf
+++ terraform/serviceaccount.tf
@@ -76,8 +76,8 @@ data "google_service_account" "registroapp" {
   depends_on = [ google_project_service.iam ]
 }
 
-# Retrieves the service account of the registroapp microservice.
-# This is defined as part of the registroapp microservice
+# Retrieves the service account of the invoice microservice.
+# This is defined as part of the invoice microservice
 # This service account is given permissions to access this microservice
 data "google_service_account" "invoice" {
   account_id   = "invoice"
